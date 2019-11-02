6710f834a0e9501e719903fc664119647bc56952
clocknane <clocknane1981@gmail.com>
2019-11-02 11:01:21 -0500

    Add Files

---
+++ gradeBook/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace gradeBook
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			string name;
+
+			Console.WriteLine("Hello, please enter the student name: ");
+			name = Console.ReadLine();
+
+			Dictionary<string, string[]> students = new Dictionary<string, string[]>();
+
+			students.Add("names", new string[] { "Chance" });
+			students.Add("grades", new string[] { "100 90 78 101 45 81" });
+
+			foreach (string names in students["names"])
+			{
+				Console.WriteLine(name);
+			}
+
+			Console.WriteLine("");
+			Console.WriteLine("");
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ masterMind/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace masterMind
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Random random = new Random();
+			int randomColor = random.Next();
+
+			Console.WriteLine("Welcome to Mastermind!");
+		}
+	}
+}
